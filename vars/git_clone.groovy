9aff6b1f786d0399adaf2fbd6b5eca7394686a9f
athix-d <atharv2686@gmail.com>
2026-05-02 13:21:53 +0530

    Fix function definition syntax in git_clone.groovy

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,4 +1,4 @@
-def call cloneGitRepo(String repoUrl, String branch) {
+def call(String repoUrl, String branch) {
     echo "Clonning the code"
     git url: repoUrl, branch: branchName
     echo "Code Cloned successfully"
